3e893fea700d23370cfd05fd9f264f4a273a222b
Elijah Samuels <elijahsamuels@gmail.com>
2021-03-29 13:10:48 -0600

    commit tree 03ca561d5050cfa2c7fe3181c9ccb79109bc74b6

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,41 @@
-export function manageFriends(state, action){
+export function manageFriends(state = {friends: []}, action){
+
+	switch (action.type) {
+		case 'ADD_FRIEND':
+			return {
+				...state, 
+				friends: [
+					...state.friends,
+					action.friend
+				]
+			}
+
+		case 'REMOVE_FRIEND':
+			// return all friends except the one removed
+			const removalIndex = state.friends.findIndex(friend => friend.id === action.id);
+			return {
+				...state, 
+				friends: [
+					...state.friends.slice(0, removalIndex),
+					...state.friends.slice(removalIndex + 1)
+				]
+			}
+
+		default:
+			return state;
+		
+	}
 }
+
+
+// function reducer(state, action){      
+// 	switch (action.type) {
+// 	  case 'INCREASE_COUNT':
+// 		return {count: state.count + 1}
+// 	  case 'DECREASE_COUNT':
+// 		return {count: state.count - 1}
+// 	  default:
+// 		return state;
+// 	}
+//   } 
+  
